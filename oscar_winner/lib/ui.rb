a1e2e87b35249910f10da12e30013f4efec07a08
Stephen Sanchez <stephensanchez05@gmail.com>
2020-01-17 02:12:52 +0000

    version 0.4.0

--- oscar_winner/lib/ui.rb
+++ oscar_winner/lib/ui.rb
@@ -44,13 +44,9 @@ class UI
     puts ""
     puts "Please enter the number of the category you would like to see (1-24)"
     input = gets.strip.to_i
-    if input.is_a?(Integer)
     Scraper.new.specific_oscar(input)
-    else
-    selection
     end
   end
-end
-    
-    
+
+
 UI.new.start
